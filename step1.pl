8eff8791542324b725c686a9bbb5667fc30fca93
Yanael Barbier <ybarbier@student.42.fr>
2015-03-29 03:45:53 +0200

    Add beauperre et bellemere

--- step1.pl
+++ step1.pl
@@ -1,4 +1,6 @@
 femme(X) :- member(X, [anne, betty, sylvie, lisa, julie, valerie, eve]).
 homme(X) :- member(X, [marc, luc, jean, jules, leon, loic, gerard, jacques, herve, paul]).
-marie_de(H, F) :- member((H, F), [(marc, anne), (luc, betty), (jules, lisa), (leon, sylvie), (loic, eve), (paul, julie)]).
-femme_de(F, H) :- marie_de(H, F).
+mari_de(H, F) :- member((H, F), [(marc, anne), (luc, betty), (jules, lisa), (leon, sylvie), (loic, eve), (paul, julie)]).
+femme_de(F, H) :- marie_d(H, F).
+beaupere_de(X, Y) :- member((X, Y), [(luc, eve), (luc, jules), (marc, loic), (leon, paul)]).
+bellemere_de(X, Y) :- member((X, Y), [(anne, sylvie), (anne, eve), (betty, jules), (betty, leon), (sylvie, paul), (loic, )]).
