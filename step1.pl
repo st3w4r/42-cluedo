b5e29d518d44d4f02197a43012fc4bb1c7b5f2f2
Yanael Barbier <ybarbier@student.42.fr>
2015-03-29 16:00:21 +0200

    Add Enfant de

--- step1.pl
+++ step1.pl
@@ -1,6 +1,18 @@
 femme(X) :- member(X, [anne, betty, sylvie, lisa, julie, valerie, eve]).
 homme(X) :- member(X, [marc, luc, jean, jules, leon, loic, gerard, jacques, herve, paul]).
 mari_de(H, F) :- member((H, F), [(marc, anne), (luc, betty), (jules, lisa), (leon, sylvie), (loic, eve), (paul, julie)]).
-femme_de(F, H) :- marie_d(H, F).
+femme_de(F, H) :- marie_de(H, F).
 beaupere_de(X, Y) :- member((X, Y), [(luc, eve), (luc, jules), (marc, loic), (leon, paul)]).
-bellemere_de(X, Y) :- member((X, Y), [(anne, sylvie), (anne, eve), (betty, jules), (betty, leon), (sylvie, paul), (loic, )]).
+bellemere_de(X, Y) :- member((X, Y), [(anne, sylvie), (anne, eve), (betty, jules), (betty, leon), (sylvie, paul)]).
+
+enfant_de(E, P) :- member([E|P], [[jean|marc], [jules|marc], [leon|marc]]).
+enfant_de(E, P) :- member([E|P], [[lisa|luc], [loic|luc], [gerard|luc]]).
+enfant_de(E, P) :- member([E|P], [[jacques|jules]]).
+enfant_de(E, P) :- member([E|P], [[herve|leon], [julie|leon]]).
+enfant_de(E, P) :- member([E|P], [[paul|loic], [valerie|loic]]).
+
+
+
+enfant_de(E, P) :- femme(P), femme_de(P, Y), enfant_de(E, Y).
+
+ancetre_de(A, E) :- enfant_de(E, A).
