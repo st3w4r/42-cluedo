190b562bd10cbf0a9440f6bca7c94f2de513332b
Yanael Barbier <ybarbier@student.42.fr>
2015-03-29 17:39:43 +0200

    Send step1 ancetre

--- step1.pl
+++ step1.pl
@@ -1,7 +1,9 @@
 femme(X) :- member(X, [anne, betty, sylvie, lisa, julie, valerie, eve]).
 homme(X) :- member(X, [marc, luc, jean, jules, leon, loic, gerard, jacques, herve, paul]).
+
 mari_de(H, F) :- member((H, F), [(marc, anne), (luc, betty), (jules, lisa), (leon, sylvie), (loic, eve), (paul, julie)]).
-femme_de(F, H) :- marie_de(H, F).
+femme_de(F, H) :- mari_de(H, F).
+
 beaupere_de(X, Y) :- member((X, Y), [(luc, eve), (luc, jules), (marc, loic), (leon, paul)]).
 bellemere_de(X, Y) :- member((X, Y), [(anne, sylvie), (anne, eve), (betty, jules), (betty, leon), (sylvie, paul)]).
 
@@ -10,9 +12,7 @@ enfant_de(E, P) :- member([E|P], [[lisa|luc], [loic|luc], [gerard|luc]]).
 enfant_de(E, P) :- member([E|P], [[jacques|jules]]).
 enfant_de(E, P) :- member([E|P], [[herve|leon], [julie|leon]]).
 enfant_de(E, P) :- member([E|P], [[paul|loic], [valerie|loic]]).
-
-
-
 enfant_de(E, P) :- femme(P), femme_de(P, Y), enfant_de(E, Y).
 
 ancetre_de(A, E) :- enfant_de(E, A).
+ancetre_de(A, E) :- enfant_de(E, X), ancetre_de(A, X).
